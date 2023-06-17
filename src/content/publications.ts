e561aed823abc0e8decfd7e080cf391432e450b8
pppeyo38 <fuwafuwa.melon47@gmail.com>
2023-06-17 23:58:45 +0900

    :sparkles: add publications data

---
+++ src/content/publications.ts
@@ -0,0 +1,139 @@
+export const ArticlesList = [
+  {
+    title:
+      'Melanopsin-Based Brightness Discrimination in Mice and Humans(2012)',
+    link: '',
+    translation: 'マウスとヒトにおけるメラノプシンに基づく輝度識別'
+  },
+  {
+    title:
+      'Delayed response of human melanopsin retinal ganglion cells on the pupillary light reflex(2011)',
+    link: '',
+    translation: '瞳孔光反射におけるヒトメラノプシン網膜神経節細胞の遅延性反応'
+  },
+  {
+    title:
+      'Contribution of human melanopsin retinal ganglion cells to steady-state pupil responses(2010)',
+    link: '',
+    translation: '定常瞳孔反応に対するヒトメラノプシン網膜神経節細胞の寄与度'
+  },
+  {
+    title:
+      'Two distinct cone-opponent processes in the L+M luminance pathway(2007)',
+    link: '',
+    translation: 'L+M輝度経路における2つの異なる錐体対向過程'
+  },
+  {
+    title: 'Pupil response to colour signals in cone-contrast space(2006)',
+    link: '',
+    translation: '錐体コントラスト空間における色信号に対する瞳孔の反応'
+  },
+  {
+    title:
+      'Pupil responses associated with coloured afterimages are mediated by the magnocellular pathway(2003)',
+    link: '',
+    translation: '大脳細胞経路によって媒介される色彩残像に伴う瞳孔の反応'
+  },
+  {
+    title:
+      'Similarities between visual processing of shear and uniform motion(2002)',
+    link: '',
+    translation: 'せん断運動と一様運動に対する視覚処理の類似性'
+  },
+  {
+    title: 'A linear chromatic mechanism drives the pupillary responses(2001)',
+    link: '',
+    translation: 'リニアクロマティックメカニズムにより駆動する瞳孔の反応'
+  },
+  {
+    title:
+      'Technique to investigate the temporal phase shift between L- and M-cone inputs to the luminance mechanism(2000)',
+    link: '',
+    translation:
+      '輝度機構におけるL錐体入力とM錐体入力の時間的位相差を調べる技術'
+  },
+  {
+    title:
+      'Selective cone suppression by the L-M and M-L cone-opponent mechanisms in the luminance pathway(1999)',
+    link: '',
+    translation:
+      '輝度経路におけるL-MおよびM-L錐体対向機構による選択的な錐体抑制について'
+  }
+]
+
+export const CollaborateList = [
+  {
+    heading: 'Melanopsin expressing retinal ganglion cells',
+    content: [
+      'Katsunori Okajima,School of Engineering,Yokohama National University,Japan',
+      'Akiyoshi Kitaoka,Department of Psychology,Ritsumeikan University,Japan',
+      'Robert Lucas,Faculty of Life Sciences,The University of Manchester,UK',
+      'Su-ling Yeh,National Taiwan University,Taiwan'
+    ]
+  },
+  {
+    heading: 'Pupil & Clinical research',
+    content: [
+      'Aki Kawasaki, Department of Neuro-ophthalmology, University of Lausanne, Switzerland',
+      'Martial Geiser, System engineering institute, HES-SO Valais-Wallis, Switzerland',
+      'John L Barbur,School of Community and Health Sciences,City University London,UK',
+      'James Wolffsohn,School of Life and Health Sciences,Aston University,UK'
+    ]
+  },
+  {
+    heading: 'Circadian Rhy thms',
+    content: [
+      "Takeshi Morita,Department of Living Environmental Science,Fukuoka Women's University",
+      'Shigekazu Higuchi,Faculty of Design,Kyushu University,Japan',
+      'Shuhei Hashiguchi,Department of Chemistry, Biotechnology, and Chemical Engineering Graduate School of Science and Engineering, Kagoshima University,Japan'
+    ]
+  }
+]
+
+export const MediaList = [
+  {
+    date: '2012.6.7/2012.6.14',
+    heading: '週刊新潮で研究が紹介',
+    media:
+      '「サイエンス宅配便：目の中にある第３の受容体」（サイエンスライター竹内薫さん）'
+  },
+  {
+    date: '2012/06/04',
+    heading: '日経BPネットで研究が紹介',
+    media:
+      '「照明機器やディスプレイの省エネにもつながる」、明るさ感に影響を与える仕組みを鹿児島大学らが解明」',
+    link: 'https://xtech.nikkei.com/dm/article/NEWS/20120604/221397/'
+  },
+  {
+    date: '2012/05/28',
+    heading: '南日本新聞22面で研究が紹介',
+    media: '「網膜メラノプシン神経節細胞：明るさ知覚の機能確認」'
+  },
+  {
+    date: '2011.3.26',
+    heading: '南日本新聞 「研究のおと」欄',
+    media: '「生体リズムの解明：手作り装置で細胞機能解明」'
+  },
+  {
+    date: '2010.11',
+    heading: '鹿児島大学広報誌 鹿大ジャーナルで研究が紹介',
+    media: '「メラノプシン神経節細胞の機能解明に挑む」',
+    link: 'http://www.sda.nagoya-cu.ac.jp/tsujimura/kadaiNo185_2010.pdf'
+  },
+  {
+    date: '2010.4.23',
+    heading: '科学新聞',
+    media: '「概日リズム調節に関与 新光受容器メカニズム発見」'
+  },
+  {
+    date: '2010.6.11',
+    heading: '南日本新聞',
+    media: '「鹿児島知の最前線：時差ぼけを早期修正.新装置で神経細胞を刺激」'
+  },
+  {
+    date: '2008.8',
+    heading: '積水化学工業',
+    media: '「自然に学ぶ研究事例：自然光に学ぶ多原色光源装置の開発」',
+    link: 'https://www.sekisui.co.jp/sustainability_report/'
+  }
+]
