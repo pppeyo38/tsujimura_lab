73f10de3714f0d55da339c92b23549c1ff33d5a4
pppeyo38 <fuwafuwa.melon47@gmail.com>
2023-07-03 17:34:55 +0900

    :shower: movie

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 
+import { css } from '@linaria/core'
+
 import { ArticleContainer } from '@/components/template/ArticleContainer'
-import { MovieContainer } from '@/components/template/MovieContainer'
-import { Heading } from '@/components/typography/Heading'
 import { Text } from '@/components/typography/Text'
 import { Title } from '@/components/typography/Title'
 
@@ -15,29 +15,14 @@ export default function ForStudents() {
 
       <ArticleContainer>
         <Title>For Students</Title>
-        <section>
-          <Heading>研究室配属</Heading>
-          <Text>
-            辻村研究室の配属申請は【情報・産業ともに6/30(金)まで】です。
-            産業の申請期間中のみとなります。
-            情報の申請期間中には辻村研究室への申請はできませんのでご注意ください。
-          </Text>
-          <Text>
-            名古屋市立大学芸術工学部情報・産業学科３年生へ向けた辻村研究室についての紹介動画になります。
-            配属にあたっての参考になれば幸いです。
-          </Text>
-          <MovieContainer>
-            <iframe
-              width='100%'
-              height='100%'
-              src='https://www.youtube.com/embed/H_h7I3eQtBg'
-              title='YouTube video player'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-              allowFullScreen
-            />
-          </MovieContainer>
+        <section className={section}>
+          <Text>現在お知らせはありません。</Text>
         </section>
       </ArticleContainer>
     </>
   )
 }
+
+const section = css`
+  height: calc(100svh - 12rem - 304px);
+`
